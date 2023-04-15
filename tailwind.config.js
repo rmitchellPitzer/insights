499b81b5e91af7c500b257291ccce6b67aa1aa00
Ryder Mitchell <rydermitchell@Ryders-MacBook-Pro-107.local>
2023-04-15 16:19:52 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['"Inter"', ...defaultTheme.fontFamily.sans]
+      },
+    },
+  },
+  plugins: [],
+}
